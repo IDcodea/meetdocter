8c92cee84023dfbb1ee8523e98497875767dfcff
IDcodea <arismunandar1194@gmail.com>
2022-10-05 11:10:21 -0600

    update database

---
+++ database/migrations/2022_10_05_161148_create_permisson_role_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permisson_role', function (Blueprint $table) {
+            $table->id();
+            $table->integer('permisson_id');
+            $table->integer('permisson_role');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permisson_role');
+    }
+};
